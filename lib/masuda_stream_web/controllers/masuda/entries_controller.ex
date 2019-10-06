1c4ea0cf0d2e7e57a3d664dc3a7fa9ea41791627
AkiraFukushima <h3.poteto@gmail.com>
2019-10-06 16:22:39 +0900

    Add incremental search API for entries

--- lib/masuda_stream_web/controllers/masuda/entries_controller.ex
+++ lib/masuda_stream_web/controllers/masuda/entries_controller.ex
@@ -4,11 +4,25 @@ defmodule MasudaStreamWeb.Masuda.EntriesController do
   use Timex
   alias MasudaStream.Repo
 
-  def index(conn, _params) do
-    before = Timex.now()
+  def index(conn, %{"before" => before} = _params) do
+    since = before
+    |> String.to_integer(10)
+    |> Timex.from_unix()
     entries = Repo.all(
       from p in MasudaStream.Hatena.Entry,
-      where: p.posted_at <= ^before,
+      where: p.posted_at <= ^since,
+      order_by: [desc: p.posted_at],
+      limit: 20,
+      select: p
+    )
+    render(conn, "index.json", entries: entries)    
+  end
+  
+  def index(conn, _params) do
+    since = Timex.now()
+    entries = Repo.all(
+      from p in MasudaStream.Hatena.Entry,
+      where: p.posted_at <= ^since,
       order_by: [desc: p.posted_at],
       limit: 20,
       select: p
