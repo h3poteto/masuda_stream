945621b437f912f5f0cd269c960f6241b7e12cf5
AkiraFukushima <h3.poteto@gmail.com>
2019-10-12 18:22:10 +0900

    refs #24 Fix hostname

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :masuda_stream, MasudaStreamWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "masuda.stream", port: 80],
+  secret_key_base: System.get_env("SECRET_KEY"),
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
