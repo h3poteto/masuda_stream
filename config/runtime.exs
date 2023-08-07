8a06ce8926f1b5b5910021bf04c4a8dd37325ed3
AkiraFukushima <h3.poteto@gmail.com>
2023-08-07 23:13:04 +0900

    Fix listen ips

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,7 +2,7 @@ import Config
 
 if config_env() == :prod do
   config :masuda_stream, MasudaStreamWeb.Endpoint,
-    http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+    http: [ip: {0, 0, 0, 0}, port: String.to_integer(System.get_env("PORT") || "4000")],
     secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
 
   config :rollbax,
