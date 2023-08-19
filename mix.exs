58f7896fcdb77f2b5a36524ef308ab3080cf283f
AkiraFukushima <h3.poteto@gmail.com>
2023-08-19 21:59:20 +0900

    Remove ex_slack_logger

--- mix.exs
+++ mix.exs
@@ -60,7 +60,6 @@ defmodule MasudaStream.MixProject do
       {:oauther, "~> 1.1"},
       {:poison, "~> 4.0"},
       {:quantum, "~> 3.3"},
-      {:ex_slack_logger, "~> 0.1.0"},
       {:rollbax, "~> 0.11"}
     ]
   end
